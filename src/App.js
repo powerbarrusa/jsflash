e00331faf9a34e0085116fad7d2f90a4b4c4ebd2
Barr Covrigaru <barr@covrigaru.com>
2019-01-23 09:39:14 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>JS Flash!</h1>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
