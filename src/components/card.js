8d8a0aafe1412f5a8e424462c8e8b1b86cedc437
powerbarrusa <barr@covrigaru.com>
2019-02-02 11:59:31 -0700

    added delete button

--- src/components/card.js
+++ src/components/card.js
@@ -4,9 +4,12 @@ const Card = (props) => {
   return (
   <div className="container">
     <div className="card">
-        <h5 className="card-header">Guess The Method!</h5>
-        <p className="card-text">{`Method Description: ${props.description}`}</p>
-        <h4 className="card-title">{`Method Example: ${props.example}`}</h4>
+      <h5 className="card-header">Guess The Method!</h5>
+      <p className="card-text">{`Method Description: ${props.description}`}</p>
+      <h4 className="card-title">{`Method Example: ${props.example}`}</h4>
+        <div>
+          <button type="button" class="btn btn-danger d-flex justify-content-start">Delete Card</button>
+        </div>
     </div>
   </div>
   )
